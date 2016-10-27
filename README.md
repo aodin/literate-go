b9f39304749fcda684c49cc46b59caa0774a12ee
Aaron O. Ellis <aaronoellis@gmail.com>
2016-10-26 20:47:14 -0600

    First draft

---
+++ README.md
@@ -0,0 +1,12 @@
+Literate Go
+===========
+
+Presentation on interfaces for [Denver Gophers](http://www.meetup.com/Denver-Go-Language-User-Group/)
+
+View online at [go-talks.appspot.com](https://go-talks.appspot.com/github.com/aodin/literate-go/literate-go.slide)
+
+This slide deck was made using [present](https://godoc.org/golang.org/x/tools/present)
+
+    present literate-go.slide
+
+- aodin, 2016
---
+++ examples/named_parameter/named_parameter.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Config struct{ jobs int }
+
+type Modifier interface {
+	Modify(*Config) error
+}
+
+type numJob int
+
+func (nj numJob) Modify(conf *Config) error {
+	conf.jobs = int(nj)
+	return nil
+}
+
+func NumberOfJobs(n int) numJob { return numJob(n) }
+
+func New(args ...Modifier) (conf Config, err error) {
+	for _, arg := range args {
+		if err = arg.Modify(&conf); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func main() {
+	fmt.Println(New(NumberOfJobs(1), NumberOfJobs(2)))
+}
